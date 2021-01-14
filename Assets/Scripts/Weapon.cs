dcbb3ea35731e57527c1e54bdd6fc1405415c833
JSchmidt1004 <60528235+Shiftyeyes654@users.noreply.github.com>
2021-01-13 18:17:02 -0700

    First Game Engine Project
    First Commit

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Bullet bullet;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Bullet bullet = Instantiate(this.bullet, transform.position, Quaternion.identity);
+            bullet.Fire(ray.direction);
+        }
+    }
+}
